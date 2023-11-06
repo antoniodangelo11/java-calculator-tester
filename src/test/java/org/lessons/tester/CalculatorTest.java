17a41d6b400bfb322866a9b352a5a5ac74cea8bd
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-06 17:20:29 +0100

    add DisplayName

--- src/test/java/org/lessons/tester/CalculatorTest.java
+++ src/test/java/org/lessons/tester/CalculatorTest.java
@@ -1,11 +1,13 @@
 package org.lessons.tester;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
     @Test
+    @DisplayName("Somma")
     public void testAdd() {
         float result = Calculator.add(3.0f, 2.0f);
         assertEquals(5.0f, result);
@@ -14,6 +16,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Sottrazione")
     public void testSubtract() {
         float result = Calculator.subtract(3.0f, 2.0f);
         assertEquals(1.0f, result);
@@ -21,6 +24,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Divisione")
     public void testDivide() {
         float result = Calculator.divide(6.0f, 2.0f);
         assertEquals(3.0f, result);
@@ -28,6 +32,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Divisione con 0")
     public void testDivideByZero() {
         assertThrows(IllegalArgumentException.class, () -> {
             Calculator.divide(5.0f, 0.0f);
@@ -35,6 +40,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Moltiplicazione")
     public void testMultiply() {
         float result = Calculator.multiply(3.0f, 2.0f);
         assertEquals(6.0f, result);
